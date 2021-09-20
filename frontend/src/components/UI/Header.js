d3676f84d5a1f38c53e3b65b188cf01440ee782e
FSuaverdez <sfrannz@gmail.com>
2021-09-20 17:28:00 +0800

    added changes to server

--- frontend/src/components/UI/Header.js
+++ frontend/src/components/UI/Header.js
@@ -14,9 +14,9 @@ const Header = () => {
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ml-auto'>
-              <LinkContainer to='/cart'>
+              <LinkContainer to='/offers'>
                 <Nav.Link>
-                  <i className='fas fa-shopping-cart'></i> Cart
+                  <i className='fas fa-shopping-cart'></i> Offers
                 </Nav.Link>
               </LinkContainer>
               <LinkContainer to='/login'>

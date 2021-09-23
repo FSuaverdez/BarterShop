1c550db0e40d1512920bf1bf4dff5367c808a380
Frannz <57562242+FSuaverdez@users.noreply.github.com>
2021-09-23 20:06:08 +0800

    added new route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Footer from './components/UI/Footer'
 import Header from './components/UI/Header'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
+import WatchlistScreen from './screens/WatchlistScreen'
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Container>
           <Route path='/' component={HomeScreen} exact />
           <Route path='/product/:id' component={ProductScreen} />
+          <Route path='/watchlist/:id?' component={WatchlistScreen} />
         </Container>
       </main>
       <Footer />
